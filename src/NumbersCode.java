0a6bef4c2b412e7533fec8d540ecde6fb35cb1d5
gusbertoo <160844080+gusbertoo@users.noreply.github.com>
2024-03-03 17:05:37 +0000

    Update NumbersCode.java

--- src/NumbersCode.java
+++ src/NumbersCode.java
@@ -1,3 +1,23 @@
-public class NumbersCode {
-    //to be done
+import java.util.Random;
+
+public class NumbersCode extends SecretCode {
+
+    public NumbersCode(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String generateCode() {
+        player.incrementCodesAttempted();
+        Random random = new Random();
+        String code = "";
+        while (code.length() < 4) {
+            int nextDigit = random.nextInt(10);
+            String digitStr = Integer.toString(nextDigit);
+            if (!code.contains(digitStr)) {
+                code += digitStr;
+            }
+        }
+        return code;
+    }
 }
