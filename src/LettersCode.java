d653559f2f44a0d834742e361584e56f97a81f6a
gusbertoo <160844080+gusbertoo@users.noreply.github.com>
2024-03-03 17:05:02 +0000

    Update LettersCode.java

--- src/LettersCode.java
+++ src/LettersCode.java
@@ -1,3 +1,35 @@
-public class LettersCode {
-    //to be done
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class LettersCode extends SecretCode {
+
+    public LettersCode(Player player) {
+        super(player);
+    }
+
+    String wordFile = "ListOfWords.txt";
+    
+    @Override
+    public String generateCode() {
+        List<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(wordFile))) {
+            player.incrementCodesAttempted();
+            while (scanner.hasNextLine()) {
+                words.add(scanner.nextLine().trim());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+            return "";
+        }
+
+        if (!words.isEmpty()) {
+            return words.get(new Random().nextInt(words.size()));
+        } else {
+            return "";
+        }
+    }
 }
