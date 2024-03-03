ba307a9dba9dd80b3e1f956876cab78f3b941217
gusbertoo <160844080+gusbertoo@users.noreply.github.com>
2024-03-03 13:50:10 +0000

    Add files via upload

---
+++ src/RequestCodeTest.java
@@ -0,0 +1,109 @@
+package bulls_and_cows;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RequestCodeTest {
+	
+	@Test
+	//tests when request code is called for a word game
+	public void requestLetterCode() {
+		
+		//creation of Player and Game objects
+		Player p = new Player;
+		Game g = new Game(p);
+		
+		int codes_attempted = p.getCodesAttempted; //sets codes_attempted to the value of the player's codes attempted (used to test if incrementCodesAttempted() works)
+		String code = g.requestCode("lc"); //sets codes to the value of requestCode() for a word game
+		
+		assertEquals(code.length(), 4); //checks if code is of length 4
+		assertTrue(codeInFile(code)); //checks if code is held within the word codes file
+		asserEquals(codes_attempted+1, p.getCodesAttempted); //checks that the incrementCodesAttempted() method works
+		
+	}
+	
+	@Test
+	//tests when request code s called for a number game
+	public void requestNumberCode() {
+		
+		//creation of Player and Game objects
+		Player p = new Player;
+		Game g = new Game(p);
+		
+		int codes_attempted = p.getCodesAttempted; //sets codes_attempted to the value of the player's codes attempted (used to test if incrementCodesAttempted() works)
+		int code = g.requestCode("nc"); //sets codes to the value of requestCode() for a number game
+		
+		assertEquals(String.valueOf(code).length(), 4); //checks if code is of length 4, pass if it is 4 (had to turn int into String to use length())
+		assertTrue(distinctInteger(code)); //checks that all individual numbers in code are unique
+		asserEquals(codes_attempted+1, p.getCodesAttempted); //checks that the incrementCodesAttempted() method works
+		
+	}
+	
+	@Test
+	public void noWordStored() {
+		
+		Player p = new Player;
+		Game g = new Game(p);
+		g.wordFile = "noFileExists.txt"; //changes file to a non-existant file (may need to change dependent on how the file is made)
+		
+		int codes_attempted = p.getCodesAttempted; 
+		String code = g.requestCode("lc"); 
+		
+		assertEquals(code, ""); //code should equal an empty string (can change this, depends on how request code is written)
+		assertEquals(code.length(), 0); //code should be of length 0 (can change this, depends on how request code is written)
+		asserEquals(codes_attempted, p.getCodesAttempted); //incrementCodesAttempted() should not run, meaning codes attempted should be 0
+		
+	}
+	
+	// checks if the code contains only distinct numbers, i.e. 1234 == True. 1123 == False, since 1 appears twice
+	private boolean distinctInteger(int code) {
+		
+		Stack<Integer> distinct_numbers = new Stack<>(); //creation of Stack to store all distinct numbers in code
+		
+		//loop to add distinct numbers to Stack, stops when o reaches 4 (max length of code) or when a non distinct number is found
+		for (int o = 0; o < 4; o++) {
+			int individual_code_int = code % 10; //returns the last number stored in code, i.e. code == 1234, individual_code_int == 4
+			code = code / 10; //divides code by 10 for use in next iteration of loop, i.e. code == 1234 -> 123
+			if (distinct_numbers.contains(individual_code_int)) { //if statement to see if current individual_code_int is stored within stack
+				return false; //should return false as this means not every number is distinct
+			}
+			distinct_numbers.push(individual_code_int); //adds distinct number to Stack
+		}
+		
+		return true; //only distinct numbers
+	}
+	
+	//checks if the code provided is within the codes file containing all valid word codes 
+	private boolean codeInFile(String code) {
+		
+		Stack<String> codes = new Stack<>(); //creates stack that is used to temporarily store all words held within codes file
+		
+		//reading of text file
+		try {
+			File myObj = new File("wordCodes.txt");
+			Scanner myReader = new Scanner(myObj);
+
+			//loop to store all words from file into Stack, stops when the file has no more words left
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine(); //reads word from file into variable data
+				codes.push(data); //adds data variable to Stack
+			}
+			myReader.close();
+
+		//handles errors when no file is found
+		} catch (FileNotFoundException e) {
+			e.printStackTrace(); //prints details of exception that has occurred
+		}
+		
+		//if statement to find whether it is a valid code or not, i.e. code is held within the Stack
+		if (codes.contains(code)) {
+			return true;
+		} else return false;
+		
+	}
+	
+}
